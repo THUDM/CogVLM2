932495911d151163ef4ef069af49e27fc8bc5075
zR <2448370773@qq.com>
2024-05-14 22:03:31 +0800

    update finetune ds2 demo

---
+++ finetune_demo/peft_infer.py
@@ -0,0 +1,83 @@
+"""
+This is a simple chat demo using CogVLM2 PEFT finetune model in CIL.
+Just replace the model loading part with the PEFT model loading code.
+"""
+
+import torch
+from PIL import Image
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+## Loading PEFT model
+MODEL_PATH = "THUDM/CogVLM2"  # The path to the base model (read tokenizer only)
+PEFT_MODEL_PATH = "/output/checkpoint_epoch_0_step_50"  # The path to the PEFT model
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+TORCH_TYPE = torch.bfloat16 if torch.cuda.is_available() and torch.cuda.get_device_capability()[0] >= 8 else torch.float16
+tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH, trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(
+    PEFT_MODEL_PATH,
+    torch_dtype=TORCH_TYPE,
+    trust_remote_code=True,
+    device_map="auto",
+).to(DEVICE).eval()
+
+## The following code is the same as the one in basic_demo/cli_demo.py
+
+text_only_template = "A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions. USER: {} ASSISTANT:"
+
+while True:
+    image_path = input("image path >>>>> ")
+    if image_path == '':
+        print('You did not enter image path, the following will be a plain text conversation.')
+        image = None
+        text_only_first_query = True
+    else:
+        image = Image.open(image_path).convert('RGB')
+
+    history = []
+
+    while True:
+        query = input("Human:")
+        if query == "clear":
+            break
+
+        if image is None:
+            if text_only_first_query:
+                query = text_only_template.format(query)
+                text_only_first_query = False
+            else:
+                old_prompt = ''
+                for _, (old_query, response) in enumerate(history):
+                    old_prompt += old_query + " " + response + "\n"
+                query = old_prompt + "USER: {} ASSISTANT:".format(query)
+        if image is None:
+            input_by_model = model.build_conversation_input_ids(
+                tokenizer,
+                query=query,
+                history=history,
+                template_version='chat'
+            )
+        else:
+            input_by_model = model.build_conversation_input_ids(
+                tokenizer,
+                query=query,
+                history=history,
+                images=[image],
+                template_version='chat'
+            )
+        inputs = {
+            'input_ids': input_by_model['input_ids'].unsqueeze(0).to(DEVICE),
+            'token_type_ids': input_by_model['token_type_ids'].unsqueeze(0).to(DEVICE),
+            'attention_mask': input_by_model['attention_mask'].unsqueeze(0).to(DEVICE),
+            'images': [[input_by_model['images'][0].to(DEVICE).to(TORCH_TYPE)]] if image is not None else None,
+        }
+        # add any transformers params here.
+        gen_kwargs = {
+            "max_new_tokens": 2048,
+            "pad_token_id": 128002,  # avoid warning of llama3
+        }
+        with torch.no_grad():
+            outputs = model.generate(**inputs, **gen_kwargs)
+            outputs = outputs[:, inputs['input_ids'].shape[1]:]
+            response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+            print("\nCogVLM2:", response)
+        history.append((query, response))
--- finetune_demo/peft_lora.py
+++ finetune_demo/peft_lora.py
@@ -1,12 +1,31 @@
-from tqdm import tqdm
-from peft import get_peft_model, LoraConfig, TaskType
-import torch
-from torch.utils.data import Dataset, DataLoader, random_split
-from PIL import Image
+import argparse
+import gc
 import json
 import os
 import random
-from transformers import AutoModelForCausalLM, AutoTokenizer, get_linear_schedule_with_warmup
+import threading
+
+import yaml
+from PIL import Image
+import psutil
+import torch
+from accelerate import Accelerator, DeepSpeedPlugin
+from accelerate.utils import HfDeepSpeedConfig
+from torch.utils.data import Dataset, DataLoader, random_split
+from tqdm import tqdm
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    get_linear_schedule_with_warmup
+)
+from torch.utils.tensorboard import SummaryWriter
+
+from peft import get_peft_model, LoraConfig, TaskType
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class ConversationDataset(Dataset):
@@ -14,15 +33,16 @@ class ConversationDataset(Dataset):
                  root_dir,
                  tokenizer,
                  model,
-                 torch_type=torch.bfloat16,
+                 torch_type,
                  device='cuda',
-                 input_length=75,
-                 output_length=75):
+                 input_length=1024,
+                 output_length=1024
+                 ):
         self.root_dir = root_dir
         self.tokenizer = tokenizer
         self.model = model
         self.image_dir = os.path.join(root_dir, 'images')
-        self.label_dir = os.path.join(root_dir, 'labels_en')
+        self.label_dir = os.path.join(root_dir, 'labels')
         self.filenames = os.listdir(self.image_dir)
         self.input_length = input_length
         self.output_length = output_length
@@ -106,137 +126,265 @@ class ConversationDataset(Dataset):
 
         for data_key in input_data:
             if data_key in ['images']:
-                input_data[data_key] = [data.to(self.device).to(self.torch_type) for data in input_data[data_key]]
+                input_data[data_key] = [data.to(self.device).to(self.torch_type) for data in
+                                        input_data[data_key]]
             else:
                 input_data[data_key] = input_data[data_key].to(self.device)
-
-        print(input_data["input_ids"].size())
 
         return input_data
 
 
-def finetune():
-    lr = 1e-10
-    num_epochs = 5
-    batch_size = 1
-    torch_type = torch.bfloat16
-    model_path = "CogVLM2"
-    dataset_path = 'llava_instruction_single_conversation_formate'
+def b2mb(x):
+    return int(x / 2 ** 20)
 
-    tokenizer = AutoTokenizer.from_pretrained(
-        pretrained_model_name_or_path=model_path,
-        trust_remote_code=True
-    )
-    model = AutoModelForCausalLM.from_pretrained(
-        pretrained_model_name_or_path=model_path,
-        torch_dtype=torch_type,
-        trust_remote_code=True).cuda()
 
+class TorchTracemalloc:
+    def __enter__(self):
+        gc.collect()
+        torch.cuda.empty_cache()
+        torch.cuda.reset_max_memory_allocated()
+        self.begin = torch.cuda.memory_allocated()
+        self.process = psutil.Process()
+
+        self.cpu_begin = self.cpu_mem_used()
+        self.peak_monitoring = True
+        peak_monitor_thread = threading.Thread(target=self.peak_monitor_func)
+        peak_monitor_thread.daemon = True
+        peak_monitor_thread.start()
+        return self
+
+    def cpu_mem_used(self):
+        return self.process.memory_info().rss
+
+    def peak_monitor_func(self):
+        self.cpu_peak = -1
+        while True:
+            self.cpu_peak = max(self.cpu_mem_used(), self.cpu_peak)
+            if not self.peak_monitoring:
+                break
+
+    def __exit__(self, *exc):
+        self.peak_monitoring = False
+
+        gc.collect()
+        torch.cuda.empty_cache()
+        self.end = torch.cuda.memory_allocated()
+        self.peak = torch.cuda.max_memory_allocated()
+        self.used = b2mb(self.end - self.begin)
+        self.peaked = b2mb(self.peak - self.begin)
+
+        self.cpu_end = self.cpu_mem_used()
+        self.cpu_used = b2mb(self.cpu_end - self.cpu_begin)
+        self.cpu_peaked = b2mb(self.cpu_peak - self.cpu_begin)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Finetune a CogVLM model with LoRA")
+    parser.add_argument("--lr", type=float, default=1e-7, help="Learning rate")
+    parser.add_argument("--num_epochs", type=int, default=5, help="Number of epochs")
+    parser.add_argument("--batch_size", type=int, default=2, help="Batch size")
+    parser.add_argument("--torch_type", type=str, default="torch.bfloat16", help="Torch type")
+    parser.add_argument("--save_step", type=int, default=50, help="Steps between checkpoints")
+    parser.add_argument("--train_dataset_rate", type=float, default=0.8,
+                        help="Proportion of dataset to use for training")
+    parser.add_argument("--local_rank", type=int, default=-1, help="Local rank for distributed training")
+    parser.add_argument("--lora_rank", type=int, default=8, help="Rank parameter for LoRA")
+    parser.add_argument("--lora_alpha", type=int, default=32, help="Alpha parameter for LoRA")
+    parser.add_argument("--lora_target", type=str, default=["lm_head"],
+                        help="Finetune Target for LoRA")
+    parser.add_argument("--lora_dropout", type=float, default=0.1, help="Dropout rate for LoRA")
+    parser.add_argument("--warmup_steps", type=int, default=0,
+                        help="Number of warmup steps for learning rate scheduler")
+    parser.add_argument("--max_input_len", type=int, default=1024, help="Maximum input length")
+    parser.add_argument("--max_output_len", type=int, default=1024, help="Maximum output length")
+    parser.add_argument("--model_path", type=str, default="THDUM/CogVLM2",
+                        help="Path to the pretrained model")
+    parser.add_argument("--dataset_path", type=str,
+                        default="CogVLM-SFT-311K/llava_instruction_single_conversation_formate",
+                        help="Path to the conversation dataset")
+    parser.add_argument("--save_path", type=str, default="output", help="Path to save the finetuned model")
+    parser.add_argument("--ds_config", type=str, default="ds_config.yaml",
+                        help="DeepSpeed configuration file path")
+    args = parser.parse_args()
+    args.torch_type = eval(args.torch_type)
+
+    if not os.path.exists(args.save_path):
+        os.makedirs(args.save_path)
+
+    with open(args.ds_config) as f:
+        ds_config = yaml.safe_load(f)
+    hf_ds_config = HfDeepSpeedConfig(ds_config)
+
+    ds_plugin = DeepSpeedPlugin(hf_ds_config=hf_ds_config)
+    accelerator = Accelerator(deepspeed_plugin=ds_plugin)
+    zero_stage = ds_plugin.hf_ds_config.config['zero_optimization']['stage']
+    is_ds_zero_3 = zero_stage == 3
+
+    tokenizer = AutoTokenizer.from_pretrained(args.model_path, trust_remote_code=True)
+    model = AutoModelForCausalLM.from_pretrained(args.model_path, torch_dtype=args.torch_type, trust_remote_code=True)
+
+    if len(tokenizer) != model.get_input_embeddings().weight.size(0):
+        model.resize_token_embeddings(len(tokenizer))
     dataset = ConversationDataset(
-        root_dir=dataset_path,
+        root_dir=args.dataset_path,
         tokenizer=tokenizer,
         model=model,
-        torch_type=torch_type
+        torch_type=args.torch_type,
+        input_length=args.max_input_len,
+        output_length=args.max_output_len
     )
-
-    train_size = int(0.8 * len(dataset))
+    train_size = int(args.train_dataset_rate * len(dataset))
     val_size = len(dataset) - train_size
     train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
 
     train_dataloader = DataLoader(
         train_dataset,
-        batch_size=batch_size,
+        batch_size=args.batch_size,
         shuffle=True,
         collate_fn=dataset.custom_collate_fn,
     )
-
     eval_dataloader = DataLoader(
         val_dataset,
-        batch_size=batch_size,
+        batch_size=args.batch_size,
         shuffle=True,
         collate_fn=dataset.custom_collate_fn,
     )
-
     peft_config = LoraConfig(
         task_type=TaskType.CAUSAL_LM,
         inference_mode=False,
-        r=8,
-        target_modules=
-        [
-            # "query_key_value", # vit
-            # "vision_expert_query_key_value", # vit
-            # "language_expert_query_key_value"  # language's attention
-            # "vision_expert_query_key_value",
-            # "vision_expert_dense",
-            "language_expert_query_key_value",
-            # "language_expert_dense"
-        ],
-        lora_alpha=32,
-        lora_dropout=0.1,
+        r=args.lora_rank,
+        target_modules=args.lora_target,
+        lora_alpha=args.lora_alpha,
+        lora_dropout=args.lora_dropout,
         bias="none",
     )
+
     model = get_peft_model(model, peft_config)
     model.print_trainable_parameters()
-
-    optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr)
     lr_scheduler = get_linear_schedule_with_warmup(
         optimizer=optimizer,
-        num_warmup_steps=0,
-        num_training_steps=(len(train_dataloader) * num_epochs),
+        num_warmup_steps=args.warmup_steps,
+        num_training_steps=(len(train_dataloader) * args.num_epochs),
+    )
+    model, train_dataloader, eval_dataloader, optimizer, lr_scheduler = accelerator.prepare(
+        model, train_dataloader, eval_dataloader, optimizer, lr_scheduler
     )
 
-    for epoch in range(num_epochs):
-        model.train()
-        total_loss = 0
-        for step, batch in enumerate(tqdm(train_dataloader)):
-            outputs = model(
-                input_ids=batch['input_ids'],
-                token_type_ids=batch['token_type_ids'],
-                attention_mask=batch['attention_mask'],
-                images=batch['images'],
-                labels=batch['labels']
-            )
+    for param in model.model.vision.parameters():
+        param.requires_grad = False
 
-            loss = outputs.loss
-            print(f"{loss=}")
-            total_loss += loss.detach().float()
-            loss.backward()
-            optimizer.step()
-            lr_scheduler.step()
-            optimizer.zero_grad()
 
-        model.eval()
-        eval_loss = 0
-        eval_preds = []
-        for step, batch in enumerate(tqdm(eval_dataloader)):
-            with torch.no_grad():
-                outputs = model(**batch)
-            loss = outputs.loss
-            eval_loss += loss.detach().float()
-            eval_preds.extend(
-                tokenizer.batch_decode(
-                    torch.argmax(
-                        outputs.logits, -1).detach().cpu().numpy(),
-                    kip_special_tokens=True
+    logger.info("Preparation done. Starting training...")
+
+    writer = SummaryWriter(log_dir=args.save_path)  # TensorBoard writer
+
+    for epoch in range(args.num_epochs):
+        with TorchTracemalloc() as tracemalloc:
+            model.train()
+            total_loss = 0
+
+            for step, batch in enumerate(tqdm(train_dataloader)):
+                outputs = model(
+                    input_ids=batch['input_ids'],
+                    token_type_ids=batch['token_type_ids'],
+                    attention_mask=batch['attention_mask'],
+                    images=batch['images'],
+                    labels=batch['labels']
                 )
-            )
+                loss = outputs.loss
+                total_loss += loss.detach().float()
+                accelerator.backward(loss)
+                optimizer.step()
+                lr_scheduler.step()
+                optimizer.zero_grad()
 
-        eval_epoch_loss = eval_loss / len(eval_dataloader)
-        eval_ppl = torch.exp(eval_epoch_loss)
+                if (step + 1) % args.save_step == 0:
+                    print(f"Epoch {epoch}, Step {step + 1}, Loss {loss.item()}")
+                    checkpoint_path = os.path.join(args.save_path, f'checkpoint_epoch_{epoch}_step_{step + 1}')
+                    model.save_pretrained(
+                        save_directory=checkpoint_path,
+                        safe_serialization=True,
+                        save_embedding_layers=True,
+                    )
+
+                writer.add_scalar('Train/Loss', loss.item(), epoch * len(train_dataloader) + step)
+
+        accelerator.print(f"GPU Memory before entering the train : {b2mb(tracemalloc.begin)}")
+        accelerator.print(f"GPU Memory consumed at the end of the train (end-begin): {tracemalloc.used}")
+        accelerator.print(f"GPU Peak Memory consumed during the train (max-begin): {tracemalloc.peaked}")
+        accelerator.print(
+            f"GPU Total Peak Memory consumed during the train (max): {tracemalloc.peaked + b2mb(tracemalloc.begin)}"
+        )
+
+        accelerator.print(f"CPU Memory before entering the train : {b2mb(tracemalloc.cpu_begin)}")
+        accelerator.print(f"CPU Memory consumed at the end of the train (end-begin): {tracemalloc.cpu_used}")
+        accelerator.print(f"CPU Peak Memory consumed during the train (max-begin): {tracemalloc.cpu_peaked}")
+        accelerator.print(
+            f"CPU Total Peak Memory consumed during the train (max): {tracemalloc.cpu_peaked + b2mb(tracemalloc.cpu_begin)}"
+        )
         train_epoch_loss = total_loss / len(train_dataloader)
         train_ppl = torch.exp(train_epoch_loss)
-        print(f"{epoch=}: {train_ppl=} {train_epoch_loss=} {eval_ppl=} {eval_epoch_loss=}")
+        accelerator.print(f"{epoch=}: {train_ppl=} {train_epoch_loss=}")
 
-        # 保存检查点
-        torch.save({
-            'epoch': epoch,
-            'model_state_dict': model.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
-        }, f'checkpoint_epoch_{epoch}.pth.tar')
+        writer.add_scalar('Train/Perplexity', train_ppl, epoch)
+        writer.add_scalar('Train/Epoch_Loss', train_epoch_loss, epoch)
 
-    # 最终保存模型
-    model.save_pretrained("output_dir")
+        model.eval()
+        eval_preds = []
+        with TorchTracemalloc() as tracemalloc:
+            for _, batch in enumerate(tqdm(eval_dataloader)):
+                batch = {k: v for k, v in batch.items() if k != "labels"}
+                with torch.no_grad():
+                    outputs = accelerator.unwrap_model(model).generate(
+                        **batch, synced_gpus=is_ds_zero_3, max_new_tokens=args.max_output_len
+                    )
+                outputs = accelerator.pad_across_processes(outputs, dim=1, pad_index=tokenizer.pad_token_id)
+                preds = accelerator.gather_for_metrics(outputs)
+                preds = preds[:, args.max_input_len + args.max_output_len:].detach().cpu().numpy()
+                eval_preds.extend(tokenizer.batch_decode(preds, skip_special_tokens=True))
+
+        accelerator.print(f"GPU Memory before entering the eval : {b2mb(tracemalloc.begin)}")
+        accelerator.print(f"GPU Memory consumed at the end of the eval (end-begin): {tracemalloc.used}")
+        accelerator.print(f"GPU Peak Memory consumed during the eval (max-begin): {tracemalloc.peaked}")
+        accelerator.print(
+            f"GPU Total Peak Memory consumed during the eval (max): {tracemalloc.peaked + b2mb(tracemalloc.begin)}"
+        )
+
+        accelerator.print(f"CPU Memory before entering the eval : {b2mb(tracemalloc.cpu_begin)}")
+        accelerator.print(f"CPU Memory consumed at the end of the eval (end-begin): {tracemalloc.cpu_used}")
+        accelerator.print(f"CPU Peak Memory consumed during the eval (max-begin): {tracemalloc.cpu_peaked}")
+        accelerator.print(
+            f"CPU Total Peak Memory consumed during the eval (max): {tracemalloc.cpu_peaked + b2mb(tracemalloc.cpu_begin)}"
+        )
+
+        correct = 0
+        total = 0
+        assert len(eval_preds) == len(
+            dataset["train"]["text_label"]
+        ), f"{len(eval_preds)} != {len(dataset['train']['text_label'])}"
+        for pred, true in zip(eval_preds, dataset["train"]["text_label"]):
+            if pred.strip() == true.strip():
+                correct += 1
+            total += 1
+        accuracy = correct / total * 100
+        accelerator.print(f"{accuracy=}")
+        accelerator.print(f"{eval_preds[:10]=}")
+        accelerator.print(f"{dataset['train']['text_label'][:10]=}")
+
+        writer.add_scalar('Eval/Accuracy', accuracy, epoch)
+        writer.add_scalar('Eval/Perplexity', torch.exp(train_epoch_loss), epoch)
+        writer.add_scalar('Eval/Epoch_Loss', train_epoch_loss, epoch)
+
+    checkpoint_path = os.path.join(args.save_path, 'final_model')
+    model.save_pretrained(
+        save_directory=checkpoint_path,
+        safe_serialization=True,
+        save_embedding_layers=True
+    )
+    accelerator.wait_for_everyone()
+    writer.close()
 
 
 if __name__ == "__main__":
-    finetune()
+    main()
