f09125978c8c134bb3a0b4fd5c3c555fe8fdb9a1
Ddggdd135 <1306334428@qq.com>
2024-06-10 19:27:18 +0800

    撤销对模型名的修改

--- basic_demo/openai_api_demo.py
+++ basic_demo/openai_api_demo.py
@@ -17,7 +17,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, TextIteratorStream
 from PIL import Image
 from io import BytesIO
 
-MODEL_PATH = 'THUDM/cogvlm2-llama3-chinese-chat-19B'
+MODEL_PATH = 'THUDM/cogvlm2-llama3-chat-19B'
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 TORCH_TYPE = torch.bfloat16 if torch.cuda.is_available() and torch.cuda.get_device_capability()[
         0] >= 8 else torch.float16
